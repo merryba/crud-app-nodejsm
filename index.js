adfe8b39fbee75f642864242e39f0e85f4be3889
Selva Jothi <hfs@Haydens-MacBook-Pro.local>
2019-11-10 13:55:17 -0800

    modified:   index.js 	modified:   views/product_view.hbs

--- index.js
+++ index.js
@@ -55,23 +55,7 @@ app.post('/save',(req, res) => {
   });
 });
 
-//route for update data
-app.post('/update',(req, res) => {
-  let sql = "UPDATE product SET product_name='"+req.body.product_name+"', product_price='"+req.body.product_price+"' WHERE product_id="+req.body.id;
-  let query = conn.query(sql, (err, results) => {
-    if(err) throw err;
-    res.redirect('/');
-  });
-});
 
-//route for delete data
-app.post('/delete',(req, res) => {
-  let sql = "DELETE FROM product WHERE product_id="+req.body.product_id+"";
-  let query = conn.query(sql, (err, results) => {
-    if(err) throw err;
-      res.redirect('/');
-  });
-});
 
 //server listening
 app.listen(port, () => {
